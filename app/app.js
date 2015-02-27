59cd3535db3bdca016a9aca0c264a40f4466c42c
Adam Benzan <adam.benzan@gmail.com>
2015-02-26 19:44:59 -0500

    add remotely toggle

--- app/app.js
+++ app/app.js
@@ -3,25 +3,27 @@
 (function(React, _) {
   var Crossword = require('./components/Crossword.jsx'),
       CrosswordModel = require('./models/CrosswordModel.js'),
-      data = require('./data.js');
-
-
+      data = require('./data.js')
+    getRemotely = true;
   
-   document.onready = function() {
-        var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
-        React.render(<Crossword model={model} rawData={data} title={data.gridinfo.name} clues={data.clues} numbered={data.numbered} cells={data.cells} size={data.gridinfo.size}/>, document.getElementById('app'));
-   };
-  /*
+
+  if (!getRemotely) {
+    document.onready = function() {
+      var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
+      React.render(<Crossword model={model} rawData={data} title={data.gridinfo.name} clues={data.clues} numbered={data.numbered} cells={data.cells} size={data.gridinfo.size}/>, document.getElementById('app'));
+    };
+  } else {
    $.ajax({
      url: "http://cruciverbalizer.com/jsonrand/9",
      datatype: "json",
      success: function(result) {
-     var data = JSON.parse(result);
-     var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
-     React.render(<Crossword model={model} rawData={data} title={data.gridinfo.name} clues={data.clues} numbered={data.numbered} cells={data.cells} size={data.gridinfo.size}/>, document.getElementById('app'));
 
+       var data = JSON.parse(result);
+       var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
+       React.render(<Crossword model={model} rawData={data} title={data.gridinfo.name} clues={data.clues} numbered={data.numbered} cells={data.cells} size={data.gridinfo.size}/>, document.getElementById('app'));
+       $('.loading').css({ display: "none" });
      }
    });
-  
-  */
+  }
+
 }(React, _));
