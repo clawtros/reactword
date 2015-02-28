af824d2a2978752386a776e4e96b69edeb1b028b
Adam Benzan <adam.benzan@gmail.com>
2015-02-28 14:02:09 -0500

    extra silly loading indicator

--- dist/app.js
+++ dist/app.js
@@ -5,29 +5,44 @@
   var Crossword = require('./components/Crossword.jsx'),
       CrosswordModel = require('./models/CrosswordModel.js'),
       data = require('./data.js'),
-      getRemotely = true, testLoading = false;
+      getRemotely = true,
+      testLoading = true;
   
 
   if (!getRemotely) {
-    document.onready = function() {
-        var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
-        React.render(React.createElement(Crossword, {model: model, rawData: data, title: data.gridinfo.name, clues: data.clues, numbered: data.numbered, cells: data.cells, size: data.gridinfo.size}), document.getElementById('app'));
-       $('.loading').addClass('out');
-       $('#app').removeClass('out');
-
-    };
+      document.onready = function() {
+          var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
+          React.render(React.createElement(Crossword, {model: model, rawData: data, title: data.gridinfo.name, clues: data.clues, numbered: data.numbered, cells: data.cells, size: data.gridinfo.size}), document.getElementById('app'));
+          $('.loading').addClass('out');
+          $('#app').removeClass('out');
+      };
   } else {
-   $.ajax({
-     url: "http://cruciverbalizer.com/jsonrand/99",
-     datatype: "json",
-     success: function(result) {
-       var data = JSON.parse(result);
-       var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
-       React.render(React.createElement(Crossword, {model: model, rawData: data, title: data.gridinfo.name, clues: data.clues, numbered: data.numbered, cells: data.cells, size: data.gridinfo.size}), document.getElementById('app'));
-       $('.loading').addClass('out');
-       $('#app').removeClass('out');
-     }
-   });
+      document.onready = function() {
+          var container = $('#loading_container'),
+              interval;
+          for (var i = 0; i < 100; i++) {
+              container.append('<div class="foo"></div>');
+          }
+          
+          function randomlyFlip() {
+              var target = $($('.foo')[parseInt(Math.random() * 100)]);
+              target.toggleClass('highlighted');
+          }
+          
+          interval = setInterval(randomlyFlip, 100);
+          $.ajax({
+              url: "http://cruciverbalizer.com/jsonrand/99",
+              datatype: "json",
+              success: function(result) {
+                  var data = JSON.parse(result);
+                  var model = new CrosswordModel(data.cells, data.gridinfo.size, data);
+                  React.render(React.createElement(Crossword, {model: model, rawData: data, title: data.gridinfo.name, clues: data.clues, numbered: data.numbered, cells: data.cells, size: data.gridinfo.size}), document.getElementById('app'));
+                  $('.loading').addClass('out');
+                  $('#app').removeClass('out');
+                  clearInterval(interval);
+              }
+          });
+      };
   }
 
 }(React, _));
